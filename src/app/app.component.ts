fa4e5c9459d3acd9570123fb4158fe09a7c4cb4f
psbrunosouza <psbrunosouza@gmail.com>
2022-03-11 15:02:57 -0300

    feat: angular class 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'projeto-curso';
+export class AppComponent implements OnInit{
+  title = "";
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.loadTitle();
+  }
+
+  loadTitle(): void {
+    this.title = "Custo angular 1"
+  }
+
+  changeTitle(event: any): void {
+    this.title = event;
+  }
 }
