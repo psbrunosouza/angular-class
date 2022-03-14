9d3641bd1f327d424e267d0ff4cbbc3ca3e74037
igorgabriel <igor32612@fiec.edu.br>
2022-03-14 17:25:35 -0300

    140322

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,21 +5,27 @@ import {Component, OnInit} from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
-  title = "";
+export class AppComponent implements OnInit {
+  nome = "";
+  idade: number;
+  CPF: number;
+  email: string;
 
   constructor() {
   }
 
   ngOnInit(): void {
-    this.loadTitle();
   }
 
-  loadTitle(): void {
-    this.title = "Custo angular 1"
+  submit() {
+    console.log(this.nome, this.idade, this.CPF, this.email)
   }
-
-  changeTitle(event: any): void {
-    this.title = event;
+  teste(){
+    if (this.idade < 18){
+      console.log('Menor de idade')
+    } else {
+      console.log('maior de idade')
+    }
   }
 }
+
