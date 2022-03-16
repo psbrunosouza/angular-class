4e2b66bd136b898f78b26325d381eec6dde83849
igorgabriel <igor32612@fiec.edu.br>
2022-03-16 16:01:35 -0300

    1603

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PageComponent } from './page/page.component';
 import {FormsModule} from "@angular/forms";
+import {NgxMaskModule} from "ngx-mask";
 
 @NgModule({
   declarations: [
     AppComponent,
     PageComponent
   ],
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        NgxMaskModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
