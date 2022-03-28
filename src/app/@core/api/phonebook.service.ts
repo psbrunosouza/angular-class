5063c394580764d63edf9dcb86464d3d97e220e0
igorgabriel <igor32612@fiec.edu.br>
2022-03-28 16:54:56 -0300

    delete pronto, falta fazer o create e o upload

--- src/app/@core/api/phonebook.service.ts
+++ src/app/@core/api/phonebook.service.ts
@@ -1,14 +1,31 @@
-import { Injectable } from '@angular/core';
+import {EventEmitter, Injectable, Output} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {PhoneBookModel} from "../model/phone-book.model";
+import {Observable} from "rxjs";
 
-@Injectable({
-  providedIn: 'root'
-})
-export class PhonebookService {
+@Injectable()
+export class PhoneBookService {
+  baseUrl = "http://localhost:3000"
 
 
-  constructor(private httpClient: HttpClient) {
+  constructor(private http: HttpClient) {}
 
+  //Cria um card de numero de telefone no bookFone
+  addPhone(phone: PhoneBookModel): Observable<PhoneBookModel>{
+    return this.http.post<PhoneBookModel>(`${this.baseUrl}/phonebook`, phone)
+  }
+
+  //Lê a lista de numeros de telefone(bookFone)
+  listPhonebook(): Observable<PhoneBookModel[]> {
+    return this.http.get<PhoneBookModel[]>(`${this.baseUrl}/phonebook`);
+  }
+
+  //Atualiza os dados com valores pré definidos
+
+
+  //Deleta o card de numero por meio do selectedPhone --> que é obtido pelo parametro do button no click
+  deleteTel(selectedPhone: number): Observable<PhoneBookModel>{
+    return this.http.delete<PhoneBookModel>(`${this.baseUrl}/phonebook/${selectedPhone}`);
   }
 
 
---
+++ src/app/pages/phone-book/phone-item/phone-item.component.ts
@@ -0,0 +1,41 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {PhoneBookModel} from "../../../@core/model/phone-book.model";
+import {PhoneBookService} from "../../../@core/api/phonebook.service";
+
+@Component({
+  selector: 'app-phone-item',
+  templateUrl: './phone-item.component.html',
+  styleUrls: ['./phone-item.component.scss'],
+  providers: [PhoneBookService]
+})
+export class PhoneItemComponent implements OnInit {
+
+  @Input() phoneItem: PhoneBookModel;
+
+  @Input() phoneBook: PhoneBookModel[] = [];
+
+  @Output() selectPhone$: EventEmitter<number> = new EventEmitter<number>();
+
+  constructor(private phoneBookService: PhoneBookService) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  act(): void {
+    console.log("teste")
+  }
+
+  create(phoneBook: PhoneBookModel){
+    this.phoneBookService.addPhone(phoneBook).subscribe(phoneBook => console.log(phoneBook))
+  }
+
+  delete(id: number){
+    this.phoneBookService.deleteTel(id).subscribe((phone) => {console.log(phone)})
+  }
+
+
+//  editTel(selectedPhone: string){
+//     this.phoneBook = this.phoneItem.name
+//  }
+}
