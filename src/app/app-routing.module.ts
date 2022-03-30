d6da93c05def95a62f1a62daf9bb2635a66ad204
igorgabriel <igor32612@fiec.edu.br>
2022-03-30 15:24:20 -0300

    3003

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {PhoneBookComponent} from "./pages/phone-book/phone-book.component";
+import {
+  CreatePhoneItemComponent
+} from "./pages/phone-book/page-inplements/create-phone-item/create-phone-item.component";
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'phonebook',
+    pathMatch: 'full'
+  },
+
+  {
+    path: 'phonebook',
+    component: PhoneBookComponent,
+    children: [
+      {
+        path: 'CreatePhoneItem',
+        component: CreatePhoneItemComponent,
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
