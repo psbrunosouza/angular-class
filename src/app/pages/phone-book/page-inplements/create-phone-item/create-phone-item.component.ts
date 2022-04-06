07d6eddc32897ae68d76c45a0e9b3738c0e1dc50
igorgabriel <igor32612@fiec.edu.br>
2022-04-06 14:37:02 -0300

    06-04

--- src/app/pages/phone-book/page-inplements/create-phone-item/create-phone-item.component.ts
+++ src/app/pages/phone-book/page-inplements/create-phone-item/create-phone-item.component.ts
@@ -1,16 +1,16 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {FormControl} from "@angular/forms";
 import {PhoneBookModel} from "../../../../@core/model/phone-book.model";
 import{PhoneBookService} from "../../../../@core/api/phonebook.service";
 
 @Component({
   selector: 'app-create-phone-item',
   templateUrl: './create-phone-item.component.html',
-  styleUrls: ['./create-phone-item.component.scss']
+  styleUrls: ['./create-phone-item.component.scss'],
+  providers:[PhoneBookService]
 })
 export class CreatePhoneItemComponent implements OnInit {
 
-  @Input() phoneItem: PhoneBookModel;
+  phoneItem: PhoneBookModel;
 
   @Input() phoneBook: PhoneBookModel[] = [];
 
@@ -19,20 +19,12 @@ export class CreatePhoneItemComponent implements OnInit {
 
   constructor(private phoneBookService: PhoneBookService) {
   }
-  id = new FormControl();
-  name = new FormControl('');
-  phone = new FormControl('');
-  status = new FormControl('');
-  cpf = new FormControl('');
-  isInternational = new FormControl(false);
-  idade = new FormControl();
-  auxiliarPhones = new FormControl('');
 
   ngOnInit(): void {
+    this.phoneItem = new PhoneBookModel();
   }
 
-  create(Phone: PhoneBookModel){
-    this.phoneBookService.addPhone(this.phoneItem).subscribe(phoneBook => console.log(this.phone))
+  create(){
+    this.phoneBookService.addPhone(this.phoneItem).subscribe()
   }
-
 }
