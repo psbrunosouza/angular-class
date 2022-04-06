36ca0b74ad42a1322d4c3f1d60566e0a4776946b
igorgabriel <igor32612@fiec.edu.br>
2022-04-06 14:37:02 -0300

    06-04

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,7 +17,7 @@ const routes: Routes = [
     component: PhoneBookComponent,
   },
   {
-    path: 'CreatePhoneItem',
+    path: 'createPhoneItem',
     component: CreatePhoneItemComponent,
   },
   {
